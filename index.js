4b945cd33b8e37676e62ff9bb15acba38b71218e
mertkaann <mert525kaan@gmail.com>
2022-12-06 17:56:22 +0300

    css ve js dosyalarındaki problem düzeltildi

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express'); //Import the express dependency
 const app = express();              //Instantiate an express app, the main work horse of this server
 const PORT = process.env.PORT || 3030;             //Save the port number where your server will be listening
 
+app.use("/assets", express.static('./assets/'));
+
 //Idiomatic expression in express to route and respond to a client request
 app.get('/', (req, res) => {        //get requests to the root ("/") will route here
     res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
